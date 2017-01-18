461ffb2eb597ecda17fdd8890591917eacc6125d
mmeder2016 <mmeder2016@gmail.com>
2017-01-17 19:53:18 -0600

    Added the post to the survey.html file and some code to log the post to apiRoutes.js.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require("path");
 
 var apiRoutes = express.Router();
+var people = [];
 
 apiRoutes.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
@@ -15,6 +16,13 @@ apiRoutes.get('/friends', function(req, res) {
 
 apiRoutes.post('/friends', function(req, res) {
     console.log("apiRoutes.post(\'/friends\', function(req, res) {");
+
+    var person = req.body;
+    person.routeName = person.name.replace(/\s+/g, "").toLowerCase();
+    console.log(person);
+    people.push(person);
+    res.json(person);
+
     res.send("apiRoutes.post(\'/friends\', function(req, res) {");
 });
 
